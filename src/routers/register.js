bac65e173d3f86a9fb2850fbc5cfa30cc1d4eb14
luisvalverd <lvalverde4199@uta.edu.ec>
2021-09-09 11:08:57 -0500

    agregated styles register

--- src/routers/register.js
+++ src/routers/register.js
@@ -1,20 +1,25 @@
 import { Component } from "react";
+import "../components/styles/register.css";
 
 export default class Register extends Component {
   render() {
     return (
-      <div className="register">
-        <form>
-          <input type="text" className="firts_name" />
-          <imput type="text" className="last_name" />
-          <input type="email" className="email" name="email" />
-          <input type="number" className="credit_card" />
-          <input type="" className="type_credit_card" />
-          <input type="password" className="password" />
-          <input type="password" className="confirm_password" />
+      <div className="body">
+        <div className="register-card">
+          <form>
+            <input type="text" className="firts_name" />
+            <imput type="text" className="last_name" />
+            <input type="email" className="email" name="email" />
+            <input type="number" className="credit_card" />
+            <input type="" className="type_credit_card" />
+            <input type="password" className="password" />
+            <input type="password" className="confirm_password" />
 
-          <button type="submit">Sign up</button>
-        </form>
+            <button type="submit" className="sign-up">
+              Sign up
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
